953694d6379d523f9c586c9b67f2247e30aeeab9
Ivan Mudarth <ivan.mudarth@yahoo.com>
2020-05-30 18:23:01 -0400

    chrome extension build

---
+++ build/precache-manifest.abcbf5ba7666034f693c8b3a8e9a3bb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eded7f439050b84ed2a7c7844349a65e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0a24420dfdd8af9ad0a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c9b7eb607aba6b18847",
+    "url": "/static/js/2.cea29156.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cea29156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a24420dfdd8af9ad0a",
+    "url": "/static/js/main.0ebdffa3.chunk.js"
+  },
+  {
+    "revision": "b11643ed4cdbbd2c4c32",
+    "url": "/static/js/runtime-main.22b8eeb3.js"
+  }
+]);
